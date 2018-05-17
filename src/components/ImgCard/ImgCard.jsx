890f86f49f16afaf152014b545728e7bd1b77de0
Roland Willis <rolandwillisiv@gmail.com>
2018-05-16 22:51:50 -0700

    removed debugging tests

--- src/components/ImgCard/ImgCard.jsx
+++ src/components/ImgCard/ImgCard.jsx
@@ -5,7 +5,7 @@ const ImgCard = props => {
 
     return (
         <div>
-            <img id={props.id} className="img-thumbnail" src={props.imgSrc} alt={props.name} onClick={() => props.setToClicked(props.id)}/><span>{props.id}+{props.clck}</span>
+            <img id={props.id} className="img-thumbnail" src={props.imgSrc} alt={props.name} onClick={() => props.setToClicked(props.id)}/>
         </div>
     );
 }
