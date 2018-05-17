05b866d9ff142e9169167a9e3b18392dcc69b041
Roland Willis <rolandwillisiv@gmail.com>
2018-05-16 22:30:32 -0700

    updated scoreboard

--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ScoreBoard = props => (
         <div className="jumbotron">
-            <h1>Hello</h1>
+            <h1>Goodbye</h1>
             <h2>Youre Current Score: {props.score} </h2>
             <h2>CUrrent High Score: {props.topScore} </h2>
         </div>
